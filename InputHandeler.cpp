93cbb6718b64dbd698dc21a5623ec96ee9a17c0b
mike <mikailbg2022@gmail.com>
2026-02-17 12:07:50 +0330

    inputhandler and save load created

---
+++ InputHandeler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LEGION on 17/02/2026.
+//
+
+#include "InputHandeler.h"
---
+++ Logger.h
@@ -0,0 +1,13 @@
+//
+// Created by LEGION on 17/02/2026.
+//
+
+#ifndef PROJECT_FOP_LOGGER_H
+#define PROJECT_FOP_LOGGER_H
+
+
+class Logger {
+};
+
+
+#endif //PROJECT_FOP_LOGGER_H
