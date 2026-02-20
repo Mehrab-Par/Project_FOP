d8ccda8b9b64669f3bf90d3bbb51416e4f230827
Mehrab <mehrab.par1314@gmail.com>
2026-02-20 14:55:18 -0800

    update saveload.h

--- SaveLoad.h
+++ SaveLoad.h
@@ -2,20 +2,8 @@
 #include "GameState.h"
 #include <string>
 
-
-namespace SaveLoad
-{
-    bool saveProject(GameState& state, const std::string& filename);
-    bool loadProject(GameState& state, const std::string& filename);
-    void newProject(GameState& state);
-
-
-    std::string serializeSprite(const Sprite& sprite);
-    bool        deserializeSprite(GameState& state, const std::string& data, Sprite& sprite);
-
-    std::string serializeBlock(const Block& block);
-    Block*      deserializeBlock(const std::string& data);
-
-    std::string serializeVariables(GameState& state);
-    void        deserializeVariables(GameState& state, const std::string& data);
+namespace SaveLoad {
+    bool        saveProject     (const GameState& state, const std::string& filename);
+    bool        loadProject     (GameState& state,       const std::string& filename);
+    std::string getDefaultSavePath();
 }
