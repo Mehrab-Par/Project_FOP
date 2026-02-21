e4087230eacd6baec049381f139e54e1c9d7f3a8
Mehrab <mehrab.par1314@gmail.com>
2026-02-21 09:49:49 -0800

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -4,14 +4,14 @@
 namespace Logger {
     static std::ofstream logFile;
     static bool initialized = false;
-    
+
     std::string getTime() {
         time_t now = time(0);
         char buf[80];
         strftime(buf, 80, "%Y-%m-%d %H:%M:%S", localtime(&now));
         return buf;
     }
-    
+
     void init(const std::string& filename) {
         logFile.open(filename, std::ios::app);
         if (logFile.is_open()) {
@@ -19,7 +19,7 @@ namespace Logger {
             info("Logger started");
         }
     }
-    
+
     void log(Level level, const std::string& message) {
         std::string prefix;
         switch(level) {
@@ -27,20 +27,20 @@ namespace Logger {
             case Level::WARNING: prefix = "[WARN] "; break;
             case Level::ERROR_LVL: prefix = "[ERROR] "; break;
         }
-        
+
         std::string msg = getTime() + " " + prefix + message;
         std::cout << msg << std::endl;
-        
+
         if (initialized && logFile.is_open()) {
             logFile << msg << std::endl;
             logFile.flush();
         }
     }
-    
+
     void info(const std::string& msg) { log(Level::INFO, msg); }
     void warning(const std::string& msg) { log(Level::WARNING, msg); }
     void error(const std::string& msg) { log(Level::ERROR_LVL, msg); }
-    
+
     void close() {
         if (initialized) {
             info("Logger closing");
